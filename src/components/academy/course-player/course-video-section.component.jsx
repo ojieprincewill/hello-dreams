9447ebc7b23eded0dce17691ed5fc1af01c36564
godwinj96 <godwinjames670@gmail.com>
2025-08-01 16:02:39 +0100

    video player

--- src/components/academy/course-player/course-video-section.component.jsx
+++ src/components/academy/course-player/course-video-section.component.jsx
@@ -4,10 +4,14 @@ import CourseTabs from "./course-tabs.component";
 import CourseInfo from "./course-info.component";
 import CourseDescription from "./course-description.component";
 
-const CourseVideoSection = ({ image, title }) => {
+const CourseVideoSection = ({ image, title, playbackId, userId, assetId, lessonTitle }) => {
   return (
     <div>
       <VideoPlayer
+        playbackId={playbackId}
+        userId={userId}
+        assetId={assetId}
+        lessonTitle={lessonTitle}
         videoUrl="https://www.w3schools.com/html/mov_bbb.mp4"
         poster={image}
         captions={null}
