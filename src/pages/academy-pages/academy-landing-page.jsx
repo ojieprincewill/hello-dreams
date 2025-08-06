82fccefcd6ec25d3a701c55ab4bf509cd87d2c7c
ojiemekeme armstrong <ojieprincewill24@gmail.com>
2025-08-06 12:29:10 +0100

    updated academy page rendering

--- src/pages/academy-pages/academy-landing-page.jsx
+++ src/pages/academy-pages/academy-landing-page.jsx
@@ -1,27 +1,16 @@
-import React from 'react';
+import React from "react";
 
-import NavBar from '../../components/landing-header/nav-bar/nav-bar.component';
-import AcademyComingSoon from '../../components/academy-coming-soon/academy-coming-soon.component';
-import FooterSection from '../../components/footer-section/footer-section.component';
-import AcademyNavbar from '../../components/academy/academy-nav/academy-nav.component';
-import AcademyHeader from '../../components/academy/academy-header/academy-header.component';
-import LandingPreviews from '../../components/academy/landing-previews/landing-previews.component';
-
-const isDev = import.meta.env.DEV;
-const isAcademyLive = isDev || import.meta.env.VITE_ACADEMY_LIVE === 'true';
+import FooterSection from "../../components/footer-section/footer-section.component";
+import AcademyNavbar from "../../components/academy/academy-nav/academy-nav.component";
+import AcademyHeader from "../../components/academy/academy-header/academy-header.component";
+import LandingPreviews from "../../components/academy/landing-previews/landing-previews.component";
 
 const AcademyLandingPage = () => {
-  return isAcademyLive ? (
+  return (
     <>
       <AcademyNavbar />
       <AcademyHeader />
       <LandingPreviews />
-      <FooterSection />
-    </>
-  ) : (
-    <>
-      <NavBar />
-      <AcademyComingSoon />
       <FooterSection />
     </>
   );
